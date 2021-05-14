b3ebb2f60e58ab1e60387814748036dad957b37d
Giorgi Sharashenidze <giorgi.sharashenidze@code.berlin>
2021-05-14 05:59:31 +0200

    Modified login function

---
+++ start.py
@@ -0,0 +1,76 @@
+import json
+import re
+
+f = open("users.json")
+users = json.load(f)["users"]
+
+def get_user_by_email(email_address: str):
+    for user in users:
+        if(user["emailAddress"] == email_address):
+            return user
+    return -1
+
+def login():
+    print("")
+    print("### Login ###")
+    print("")
+
+    
+    is_user_not_logged_in = True
+    while is_user_not_logged_in:
+
+        is_email_not_valid = True
+        email = ""
+        while is_email_not_valid:
+            email = input("Please input your email: ")
+            email_regex = '^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$'
+            if re.search(email_regex, email):
+                is_email_not_valid = False
+            else:
+                print(f'Format incorrect for: {email}')
+
+        user = get_user_by_email(email)
+        if(user == -1):
+            print(f'{email} does not exists.')
+        else:
+            print(f'Welcome {user["firstName"]} {user["lastName"]}!')
+            is_user_not_logged_in = True
+            break
+
+
+        willing_not_continue = True
+        while willing_not_continue:
+            answer = input("Do you want to try again? (y/n): ")
+            if answer.lower() == "n":
+                print("See you!")
+                return
+            elif answer.lower() == "y":
+                willing_not_continue = False
+            else:
+                print(f'{answer} is not a proper answer')
+
+
+
+print("### Welcome ###")
+print(" 1. Login")
+
+option = 0
+option_is_invalid = True
+while option_is_invalid:
+    try:
+        option = input("Please input your option: ")
+        option = int(option)
+        option_is_invalid = False
+    except:
+        print(f'{option} is not an appropriate option')
+
+if option == 1:
+    login()
+else:
+    print(f'The option {option} doesn\'t exist')
+
+
+
+# existing email => krish.lee@learningcontainer.com
+# non-existing email => non.existing@email.com
+
